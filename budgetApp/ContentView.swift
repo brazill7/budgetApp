547234d3b0ec81559cab237de6ddf1fe65bb6670
Maverick <brazillmav@gmail.com>
2023-07-18 16:06:44 -0400

    Initial Commit

---
+++ budgetApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  budgetApp
+//
+//  Created by Maverick Brazill on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ budgetApp/budgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetAppApp.swift
+//  budgetApp
+//
+//  Created by Maverick Brazill on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct budgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
